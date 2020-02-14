f4368d03f8e0766380829858d13a70f6d7ed5a8c
ZahirKali <sofiane.kali@gmail.com>
2020-02-14 22:29:08 +0100

    reglage

--- src/main/java/fr/zki/tuto/service/UserService.java
+++ src/main/java/fr/zki/tuto/service/UserService.java
@@ -3,11 +3,13 @@ package fr.zki.tuto.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import fr.zki.tuto.dao.UserDao;
 import fr.zki.tuto.model.User;
 
+@Service
 public class UserService {
 
 	@Autowired
